e02cef9fd5ebcefeca826385fa1d013d15957bab
yonghoon304 <142773920+yonghoon304@users.noreply.github.com>
2026-02-26 17:24:45 +0900

    Commit

---
+++ Test01/Test01/Test01.c
@@ -0,0 +1,24 @@
+﻿// Test01
+
+#include <stdio.h>
+
+int n;
+
+int main(void) {
+	char arr[5] = "hong";
+	char* arr1 = "hong";
+	//printf("arr : %s\n", arr);
+
+	//printf("arr[0] : %c\n", arr[0]);
+	//printf("arr[1] : %c\n", arr[1]);
+	//printf("arr[2] : %c\n", arr[2]);
+	//printf("arr[3] : %c\n", arr[3]);
+
+	printf("arr : %p\n", arr);
+	printf("arr1 : %p\n", &arr1);
+	printf("hong : %p\n", *arr1);
+	printf("n : %p\n", &n);
+
+
+	return 0;
+}
---
+++ Test01/Test02/Test02.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+
+int a;
+int global_c = 100;
+
+int func() {
+
+	return 1;
+}
+int main(void) {
+
+	int b;
+	static int c;
+	const int d=10;
+	char arr[20] = "hi";
+	char* parr = "hong";
+
+	// 자료형 별로 메모리 위치 차이
+	printf("global_a : %p\n", &a);
+	printf("local b : %p\n", & b);
+	printf("static c : %p\n", &c);
+	printf("global_c : %p\n", &global_c);
+	printf("const d : %p\n", &d);
+	printf("array : %p\n", arr);
+	printf("parr : %p\n", parr);
+
+
+
+	return 0;
+}
+
+/* 메모리구조
+	data segment : stack, heap, data
+	code segment : s
+
+*/
+
+
